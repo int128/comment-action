e8fac7e41554355f0566e312dd26ef81c3a4513b
Hidetake Iwata <int128@gmail.com>
2022-10-04 16:13:41 +0900

    Create or update a comment if exists (#263)
    * Add update-if-exists
    
    * Fix
    
    * Improve logs
    
    * Add doc
    
    * Update README.md

--- src/comment.ts
+++ src/comment.ts
@@ -4,25 +4,97 @@ import { GitHub } from '@actions/github/lib/utils'
 
 type Octokit = InstanceType<typeof GitHub>
 
-export const postComment = async (octokit: Octokit, body: string) => {
-  const pullNumbers = await inferPullRequestsFromContext(octokit)
-  for (const issue_number of pullNumbers) {
-    const { context } = github
-    core.info(`Post a comment to #${issue_number}`)
-    await octokit.rest.issues.createComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number,
-      body,
-    })
+type Inputs = {
+  body: string
+  updateIfExists: UpdateIfExistsType
+  updateIfExistsKey: string
+}
+
+export type UpdateIfExistsType = 'replace' | 'append' | undefined
+
+type PullRequest = {
+  owner: string
+  repo: string
+  issue_number: number
+}
+
+export const postComment = async (octokit: Octokit, inputs: Inputs) => {
+  const pullRequests = await inferPullRequestsFromContext(octokit)
+  for (const pullRequest of pullRequests) {
+    await createOrUpdateComment(octokit, pullRequest, inputs)
   }
 }
 
-const inferPullRequestsFromContext = async (octokit: Octokit) => {
+const createOrUpdateComment = async (octokit: Octokit, pullRequest: PullRequest, inputs: Inputs) => {
+  if (inputs.updateIfExists === undefined) {
+    core.info(`Creating a comment to #${pullRequest.issue_number}`)
+    const { data: created } = await octokit.rest.issues.createComment({
+      ...pullRequest,
+      body: inputs.body,
+    })
+    core.info(`Created a comment ${created.html_url}`)
+    return
+  }
+
+  const commentKey = `<!-- comment-action/${inputs.updateIfExistsKey} -->`
+  core.info(`Finding key ${commentKey} from comments in #${pullRequest.issue_number}`)
+  const comment = await findComment(octokit, pullRequest, commentKey)
+  if (!comment) {
+    core.info(`Key not found in #${pullRequest.issue_number}`)
+    const { data: created } = await octokit.rest.issues.createComment({
+      ...pullRequest,
+      body: `${inputs.body}\n${commentKey}`,
+    })
+    core.info(`Created a comment ${created.html_url}`)
+    return
+  }
+
+  core.info(`Key found at the comment ${comment.html_url}`)
+  let body = `${inputs.body}\n${commentKey}`
+  if (inputs.updateIfExists === 'append') {
+    body = `${comment.body}\n${body}`
+  }
+  const { data: updated } = await octokit.rest.issues.updateComment({
+    owner: pullRequest.owner,
+    repo: pullRequest.repo,
+    comment_id: comment.id,
+    body,
+  })
+  core.info(`Updated the comment ${updated.html_url}`)
+}
+
+type Comment = {
+  id: number
+  body: string
+  html_url: string
+}
+
+const findComment = async (octokit: Octokit, pullRequest: PullRequest, key: string): Promise<Comment | undefined> => {
+  const { data: comments } = await octokit.rest.issues.listCommentsForRepo({
+    ...pullRequest,
+    sort: 'created',
+    direction: 'desc',
+    per_page: 100,
+  })
+  core.info(`Found ${comments.length} comment(s) of #${pullRequest.issue_number}`)
+  for (const comment of comments) {
+    if (comment.body?.includes(key)) {
+      return { ...comment, body: comment.body }
+    }
+  }
+}
+
+const inferPullRequestsFromContext = async (octokit: Octokit): Promise<PullRequest[]> => {
   const { context } = github
   if (Number.isSafeInteger(context.issue.number)) {
-    core.info(`Use ${context.issue.number} from the current context`)
-    return [context.issue.number]
+    core.info(`Use #${context.issue.number} from the current context`)
+    return [
+      {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+      },
+    ]
   }
 
   core.info(`List pull requests associated with sha ${context.sha}`)
@@ -34,5 +106,9 @@ const inferPullRequestsFromContext = async (octokit: Octokit) => {
   for (const pull of pulls.data) {
     core.info(`  #${pull.number}: ${pull.title}`)
   }
-  return pulls.data.map((p) => p.number)
+  return pulls.data.map((p) => ({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: p.number,
+  }))
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import * as core from '@actions/core'
+import { UpdateIfExistsType } from './comment'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
   await run({
     post: core.getInput('post'),
+    updateIfExists: updateIfExists(core.getInput('update-if-exists')),
+    updateIfExistsKey: core.getInput('update-if-exists-key'),
     run: core.getInput('run'),
     postOnSuccess: core.getInput('post-on-success'),
     postOnFailure: core.getInput('post-on-failure'),
     token: core.getInput('token'),
   })
+}
+
+const updateIfExists = (s: string): UpdateIfExistsType => {
+  if (!s) {
+    return undefined
+  }
+  if (s !== 'replace' && s !== 'append') {
+    throw new Error(`update-if-exists must be replace or append`)
+  }
+  return s
 }
 
 main().catch((e) => core.setFailed(e instanceof Error ? e : String(e)))
